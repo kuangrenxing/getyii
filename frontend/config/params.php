d0b2ff6d4806aaccfedd0756ef067e57a22c8cb1
陈新 <chenxin448@126.com>
2022-11-14 16:19:48 +0800

    打赏

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'donateNode' => ['tricks'], // 开启打赏分类
+    'donateNode' => ['tricks','python'], // 开启打赏分类
     'loginNode' => ['jobs'], // 需要登录才能访问的分类
-    'donateTag' => ['求打赏', '技巧库'], // 开启标签
+    'donateTag' => ['求打赏', '技巧库','php','Java'], // 开启标签
     'postingIntervalLimit' => 3, // 限制发帖间隔，单位秒。默认是 1 个小时
     'setting' => [
         'xunsearch' => false, // true 表示开启 GetYii xunsearch 搜索功能，默认不开启
